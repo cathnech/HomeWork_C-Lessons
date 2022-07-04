1e2ebb9e649958de0a1d9f0b74414a3be897739e
cathnech <cathnech@gmail.com>
2022-07-04 21:25:39 +0300

    completed task 29

--- HW_04/task29/Program.cs
+++ HW_04/task29/Program.cs
@@ -3,3 +3,30 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+//условия пока на уточнение, пока такая реализация
+
+int[] CreateArr(int size){
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArr(int[] arr){
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length-1]}]");
+}
+
+
+Console.WriteLine("Enter a length of array:");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Enter {size} numbers for array:");
+
+PrintArr( CreateArr(size) );
+
