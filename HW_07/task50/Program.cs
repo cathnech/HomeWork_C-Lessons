b683ff9037ff93dc2c2aed5f62adf08067717efe
cathnech <cathnech@gmail.com>
2022-07-15 19:03:22 +0300

    task50 and task52  completed

--- HW_07/task50/Program.cs
+++ HW_07/task50/Program.cs
@@ -7,3 +7,47 @@
 8 4 2 4
 1, 7 -> такого числа в массиве нет
 */
+
+int[,] CreateMatrix(int m,int n){
+    int[,] array = new int[m,n];
+    var rmd = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = rmd.Next(10);
+        } 
+    }
+    return array;
+}
+void PrintMatrix(int m, int n, int[,] array){
+    
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write("|\t");
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        } 
+        Console.Write("|\n");
+    }
+}
+void CheckEl(int m, int n, int[,] array, int mi, int nj){
+    if (m<mi || n<nj){
+        Console.WriteLine("No such element in the array");    
+    }
+    else {
+        Console.WriteLine(array[mi-1,nj-1]);
+    }
+
+}
+Console.WriteLine("Enter 2d matrix size:");
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Which element do you want to find:");
+int ielem = Convert.ToInt32(Console.ReadLine()),
+    jelem = Convert.ToInt32(Console.ReadLine()); 
+int[,] arr = CreateMatrix(m,n);
+PrintMatrix(m,n,arr);
+CheckEl(m,n,arr,ielem,jelem);
--- HW_07/task52/Program.cs
+++ HW_07/task52/Program.cs
@@ -7,3 +7,46 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+int[,] CreateMatrix(int m,int n){
+    int[,] array = new int[m,n];
+    var rmd = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = rmd.Next(10);
+        } 
+    }
+    return array;
+}
+void PrintMatrix(int m, int n, int[,] array){
+    
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write("|\t");
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        } 
+        Console.Write("|\n");
+    }
+}
+void ColumnAverage(int m, int n, int[,] array){
+    for (int j = 0; j < n; j++)
+    {
+        double res = 0;
+        for (int i = 0; i < m; i++)
+        {
+            res += array[i,j];
+        } 
+        Console.WriteLine($"For column {j+1} average is {res/m}");
+    }   
+     
+}
+Console.WriteLine("Enter 2d matrix size:");
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine()); 
+int[,] arr = CreateMatrix(m,n);
+PrintMatrix(m,n,arr);
+ColumnAverage(m,n,arr);
