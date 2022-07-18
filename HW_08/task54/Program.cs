de2c770eb644d2031dac320c02935ef5ae4a8fd8
cathnech <cathnech@gmail.com>
2022-07-18 16:38:53 +0300

    task54 and task56  completed

--- HW_08/task54/Program.cs
+++ HW_08/task54/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8
+*/
+
+int[,] CreateMatrix(int m,int n){
+    int[,] array = new int[m,n];
+    var rmd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rmd.Next(10);
+        } 
+    }
+    return array;
+}
+void PrintMatrix(int[,] array){
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        } 
+        Console.Write("|\n");
+    }
+    Console.Write("\n");
+}
+int[,] SortRowMatrix(int[,] array){
+   
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int jj = 0; jj < array.GetLength(1); jj++)
+        {
+            for (int j = 0; j < array.GetLength(1)-1; j++)
+            {
+                if (array[i,j] > array[i,j + 1])
+                {
+                    int temp = array[i,j + 1];
+                    array[i,j + 1] = array[i,j];
+                    array[i,j] = temp;
+                }       
+            }   
+        }
+    }
+    return array;
+}
+
+Console.WriteLine("Enter 2d matrix size:"); 
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = CreateMatrix(m,n); 
+PrintMatrix(arr);
+int[,] resArr = SortRowMatrix(arr); 
+PrintMatrix(resArr);
---
+++ HW_08/task56/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и 
+выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[,] CreateMatrix(int m,int n){
+    int[,] array = new int[m,n];
+    var rmd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rmd.Next(10);
+        } 
+    }
+    return array;
+}
+void PrintMatrix(int[,] array){
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        } 
+        Console.Write("|\n");
+    }
+    Console.Write("\n");
+}
+int MinSumOfEllements(int[] array){
+    int minSumOfEllements = array[0],
+        res = 0;
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        if (minSumOfEllements>array[i])
+        {
+            minSumOfEllements = array[i];
+            res = i;
+        }
+    }
+    
+    return res+1;
+}
+
+int CalculateRowSum(int[,] array){
+    int[] arr = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           arr[i] += array[i,j];  
+        }
+    }
+    return MinSumOfEllements(arr);
+}
+
+Console.WriteLine("Enter 2d matrix size:"); 
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = CreateMatrix(m,n); 
+PrintMatrix(arr);
+Console.WriteLine($"The number of row with the minimum sum of elements: {CalculateRowSum(arr)}");
