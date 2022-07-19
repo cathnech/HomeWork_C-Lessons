9887b52cdeb5f19cd4b38bef45635dd2e85cc24f
cathnech <cathnech@gmail.com>
2022-07-19 20:14:08 +0300

    task59, task61  completed

--- HW_08/task59/Program.cs
+++ HW_08/task59/Program.cs
@@ -14,6 +14,7 @@
 2 2 6
 3 4 7
 */
+
 int[,] CreateMatrix(int m,int n){
     int[,] array = new int[m,n];
     var rmd = new Random();
@@ -23,6 +24,37 @@ int[,] CreateMatrix(int m,int n){
         for (int j = 0; j < array.GetLength(1); j++)
         {
             array[i,j] = rmd.Next(10);
+        } 
+    }
+    return array;
+}
+int[] FindFirstMinElement(int[,] arr){  
+    int min = arr[0,0];
+    int[] coordinats = new int[2];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j]<min)
+            {
+                min = arr[i,j];
+                coordinats = new int[]{i,j};
+            }
+        } 
+    }
+    return coordinats;
+}
+int[,] DeleteColRow(int[,] arr){
+    int[,] array = new int[arr.GetLength(0)-1,arr.GetLength(1)-1];
+    int[] coordinats = FindFirstMinElement(arr);
+
+    for (int i = 0,ii = 0; i < array.GetLength(0); i++, ii++)
+    {
+        if (i == coordinats[0]){ii++;}
+        for (int j = 0,jj = 0; j < array.GetLength(1); j++,jj++)
+        {
+            if (j == coordinats[1]){jj++;}
+            array[i,j] = arr[ii,jj];
         } 
     }
     return array;
@@ -40,3 +72,10 @@ void PrintMatrix(int[,] array){
     }
     Console.Write("\n");
 }
+Console.WriteLine("Enter 2d matrix size:"); 
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = CreateMatrix(m,n); 
+PrintMatrix(arr);
+int[,] resArr = DeleteColRow(arr); //удаление по первому наименьшему элементу массива
+PrintMatrix(resArr);
--- HW_08/task61/Program.cs
+++ HW_08/task61/Program.cs
@@ -1,3 +1,38 @@
 ﻿/*
-Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника.
+Задача 61: Вывести первые N строк треугольника Паскаля. 
+Сделать вывод в виде равнобедренного треугольника.
 */
+
+int[,] CreateMatrix(int n){
+    int[,] array = new int[n,n];
+    array[0,0] = 1;
+    array[n-1,n-1] = 1;
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        array[i,0] = 1;
+        for (int j = 1; j < array.GetLength(1)-1; j++)
+        {
+            array[i,j] = array[i-1,j]+array[i-1,j-1];
+        } 
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array){
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i,j]!=0) Console.Write(array[i,j] + "\t");
+        } 
+        Console.Write("\n");
+    }
+    Console.Write("\n");
+}
+
+Console.WriteLine("Enter number of rows N :"); 
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = CreateMatrix(n); 
+PrintMatrix(arr);
