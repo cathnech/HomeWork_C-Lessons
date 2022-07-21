08f11b468b26ad1c78c1e960ee9394ab8324b76b
cathnech <cathnech@gmail.com>
2022-07-21 12:31:25 +0300

    task66 completed

--- HW_09/task66/Program.cs
+++ HW_09/task66/Program.cs
@@ -4,3 +4,20 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+int SumNaturalNumbers(int m,int n){
+    if (m == n){ 
+        return m;
+    }
+    return m+SumNaturalNumbers(m+1,n);
+}
+Console.WriteLine("Enter M & N:"); 
+int m = Convert.ToInt32(Console.ReadLine()),
+    n = Convert.ToInt32(Console.ReadLine());
+if (m>n){
+    Console.WriteLine("Error. M can't be bigger than N");
+} 
+else
+{
+    Console.WriteLine(SumNaturalNumbers(m,n));
+}
