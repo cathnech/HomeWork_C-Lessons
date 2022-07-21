4afdd1768faaf84e120f01e04a405bad9b6619a8
cathnech <cathnech@gmail.com>
2022-07-21 12:29:15 +0300

    task64 completed

--- HW_09/task64/Program.cs
+++ HW_09/task64/Program.cs
@@ -4,3 +4,15 @@
 M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8""
 */
+
+int NaturalNumbers(int n){
+    int count = 1;
+    Console.Write(n+",");
+    if (count == n-1){ 
+        return 1;
+    }
+    return NaturalNumbers(n-count);
+}
+Console.WriteLine("Enter N:"); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(NaturalNumbers(n));
